9ca655aa65c38ccb4e25a5219b6a0197d3a59144
Sara Vieira <hey@iamsaravieira.com>
2017-11-04 12:16:50 +0000

    update travis and head

--- src/server/middleware/render.js
+++ src/server/middleware/render.js
@@ -10,8 +10,11 @@ function render (req, res) {
       <!doctype html>
       <html lang="en">
         <head>
-          <meta charset="utf-8" />
-          <meta content="width=device-width, initial-scale=1" name="viewport" />
+          <meta charset="utf-8">
+          <meta http-equiv="x-ua-compatible" content="ie=edge">
+          <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+          <link rel="me" href="https://twitter.com/NikkitaFTW" type="text/html">
+          <link rel="me" href="mailto:hey@iamsaravieira.com">
           <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/codemirror/5.23.0/theme/material.css" />
           <link href="https://fonts.googleapis.com/css?family=Roboto|Roboto+Mono" rel="stylesheet">
           <title>Sara Vieira</title>
